381095f0482090afab5f0e163261f2d05eb47a0e
Anton Mazurov <outwork.anton@gmail.com>
2019-02-07 10:31:02 +0300

    Update message output

--- Upgrade.sh
+++ Upgrade.sh
@@ -3,19 +3,18 @@
 sudo apt-get update
 clear && echo -en "\e[3J"
 LISTAPP="apt-show-versions"
-if ! dpkg -s $LISTAPP >/dev/null 2>&1
+if ! dpkg -s ${LISTAPP} >/dev/null 2>&1
 then
     tput setaf 4; echo "НЕОБХОДИМА УСТАНОВКА ДОПОЛНИТЕЛЬНОГО ПАКЕТА: $LISTAPP"; tput sgr0
-    sudo apt-get -y install $LISTAPP
+    sudo apt-get -y install ${LISTAPP}
     clear && echo -en "\e[3J"
 fi
 
 PACKAGES=$(sudo apt-show-versions -u)
 
-if [ ! -z "$PACKAGES" ]
+if [[ ! -z "$PACKAGES" ]]
 then
-    tput setaf 3; echo "ДОСТУПНО ПАКЕТОВ ДЛЯ ОБНОВЛЕНИЯ: $(echo "$PACKAGES" | wc -l)"
-    echo "$PACKAGES"; tput sgr0
+    tput setaf 3; echo "ДОСТУПНО ПАКЕТОВ ДЛЯ ОБНОВЛЕНИЯ: $(echo "$PACKAGES" | wc -l)"; tput sgr0
     UPGRADELOG=$(sudo apt-get -y dist-upgrade | tee /dev/tty)
     AUTOREMOVE=$(echo "$UPGRADELOG" | grep "sudo apt autoremove" | wc -l)
     if (($AUTOREMOVE))
